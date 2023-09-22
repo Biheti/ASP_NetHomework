0f07ec2db42288f6928c542a8567d7bf69a1fda1
biljana <biljanaslavkovska13@gmail.com>
2023-09-23 00:00:17 +0200

    HW02

---
+++ Homework02/SEDC.PizzaApp/SEDC.PizzaApp/Controllers/OrdersController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Models;
+
+namespace SEDC.PizzaApp.Controllers
+{
+
+    [Route("ListOrders")]
+    public class OrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet("Details/{id}")]
+        public IActionResult Details(int id)
+        {
+            if (id == null) 
+            { 
+                return new EmptyResult();
+            }
+
+            return View();
+        }
+
+
+        [HttpGet("JsonData/{id}")]
+        public IActionResult JsonDataDetails(int id)
+        {
+            if (id == 1)
+            {
+                Pizza pizza = new Pizza()
+                {
+                    Id = 1,
+                    Name = "Margarita",
+                    Price = 15,
+                    IsOnPromotion = true
+
+                };
+                return new JsonResult(pizza);
+
+            }
+
+
+            return new EmptyResult();
+        }
+
+
+
+
+
+    }
+}
